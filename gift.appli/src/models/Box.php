7e6fecc658f230341d935cc88fff26ce90270210
Jules <jules.holder.54@gmail.com>
2023-06-01 15:36:20 +0200

    hihi j'ai oublié d'importer

--- gift.appli/src/models/Box.php
+++ gift.appli/src/models/Box.php
@@ -3,6 +3,7 @@
 namespace gift\app\models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Box extends Model

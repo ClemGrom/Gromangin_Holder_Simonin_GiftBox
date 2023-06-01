f421b558802e581e15dc0aa400e2a7a3312550cf
Jules <jules.holder.54@gmail.com>
2023-06-01 15:23:15 +0200

    optimisation imports + changements sur packages services

--- gift.appli/src/action/PostNewCategorieAction.php
+++ gift.appli/src/action/PostNewCategorieAction.php
@@ -2,17 +2,16 @@
 
 namespace gift\app\action;
 
-use gift\app\services\prestations\PrestationsService;
+use gift\app\services\categories\CategoriesServices;
 use gift\app\services\utils\CsrfService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Slim\Views\Twig;
 use const gift\app\conf\basePath;
 
 class PostNewCategorieAction
 {
 
-    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args) : ResponseInterface
+    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
     {
         $categorie = array(
             'libelle' => $_POST['libelle'],
@@ -21,10 +20,10 @@ class PostNewCategorieAction
 
         CsrfService::check($_POST['csrf']);
 
-        $p = new PrestationsService();
+        $p = new CategoriesServices();
         $p->setNewCategorie($categorie);
 
-        return $rs->withStatus(302)->withHeader('Location',  basePath . '/categories');
+        return $rs->withStatus(302)->withHeader('Location', basePath . '/categories');
     }
 
 }

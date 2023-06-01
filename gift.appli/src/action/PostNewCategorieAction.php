b2d61d85e3da3a14e19ada03b9a5674fe57e2cbe
Nzosim <nzosim54@gmail.com>
2023-06-01 15:26:29 +0200

    j

--- gift.appli/src/action/PostNewCategorieAction.php
+++ gift.appli/src/action/PostNewCategorieAction.php
@@ -4,8 +4,12 @@ namespace gift\app\action;
 
 use gift\app\services\categories\CategoriesServices;
 use gift\app\services\utils\CsrfService;
+use gift\app\services\utils\TokenInvalid;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Routing\RouteContext;
+use Slim\Views\Twig;
 use const gift\app\conf\basePath;
 
 class PostNewCategorieAction
@@ -13,17 +17,26 @@ class PostNewCategorieAction
 
     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
     {
+
+        $post_data = $rq->getParsedBody();
+
         $categorie = array(
-            'libelle' => $_POST['libelle'],
-            'description' => $_POST['description']
+            'libelle' => $post_data['libelle'],
+            'description' => $post_data['description']
         );
 
-        CsrfService::check($_POST['csrf']);
+        try{
+            CsrfService::check($post_data['csrf']);
+        }catch(TokenInvalid $e) {
+            throw new HttpBadRequestException($rq, "token invalide");
+        }
 
         $p = new CategoriesServices();
         $p->setNewCategorie($categorie);
 
-        return $rs->withStatus(302)->withHeader('Location', basePath . '/categories');
+        $routeParser = RouteContext::fromRequest($rq)->getRouteParser();
+        $url = $routeParser->urlFor('categories');
+        return $rs->withStatus(302)->withHeader('Location', $url);
     }
 
 }

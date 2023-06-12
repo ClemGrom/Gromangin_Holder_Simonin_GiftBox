ccf369851f0ebf218eddfb1ad226a7e9ad044821
Nzosim <nzosim54@gmail.com>
2023-06-12 11:45:52 +0200

    box prefaite

---
+++ gift.appli/src/action/box/GetPrefilledBoxAction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace gift\app\action\box;
+
+use gift\app\services\box\BoxServices;
+use gift\app\services\categories\CategoriesServices;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Views\Twig;
+
+class GetPrefilledBoxAction
+{
+
+    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
+    {
+
+        $b = new BoxServices();
+        $boxes = $b->getPrefilledBox();
+
+        $view = Twig::fromRequest($rq);
+        return $view->render($rs, 'box/gift.prefilled.twig', [
+            'boxes' => $boxes
+        ]);
+    }
+
+}
--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -1,6 +1,8 @@
 <?php
 
+use gift\app\action\box\GetBoxAction;
 use gift\app\action\box\GetDeletePrestaAction;
+use gift\app\action\box\GetPrefilledBoxAction;
 use gift\app\action\user\GetLogoutAction;
 use gift\app\action\box\GetMyBoxAction;
 use gift\app\action\box\GetPayAction;
@@ -52,8 +54,8 @@ return function (\Slim\App $app): void {
     // Creation Box
     $app->get('/box/new[/]', GetNewEmptyBoxAction::class)->setName("newEmptyBox");
     $app->post('/box/new[/]', PostNewEmptyBoxAction::class);
-    // Visualiser Box
-    $app->get('/box[/]', GetMyBoxAction::class)->setName("myBox");
+    // Visualiser ma Box
+    $app->get('/box/my[/]', GetMyBoxAction::class)->setName("myBox");
     // Valider Box
     $app->get('/box/validate[/]', GetValidateBoxAction::class)->setName("validateBox");
     // Payer Box
@@ -63,6 +65,10 @@ return function (\Slim\App $app): void {
     $app->get('/box/delete[/]', GetDeletePrestaAction::class)->setName("deletePresta");
     // Ajouter une prestation à la box
     $app->get('/prestation/add', PostAddPrestationToBoxAction::class)->setName("addPrestationToBox");
+    // Afficher les box préremplies
+    $app->get('/box/prefilled[/]', GetPrefilledBoxAction::class)->setName("premadeBox");
+    // Visualiser une box
+    $app->get('/box', GetBoxAction::class)->setName("box");
 
     /*
      * USER
